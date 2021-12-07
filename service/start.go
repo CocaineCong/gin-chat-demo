957116c6af1feb15a15c958a0f06d4e222b3a44f
CocaineCong <294350394@qq.com>
2021-12-07 15:36:56 +0800

    feat: start

--- service/start.go
+++ service/start.go
@@ -22,57 +22,57 @@ func (manager *ClientManager) Start() {
 			}
 			_ = conn.Socket.WriteMessage(websocket.TextMessage, *(*[]byte)(unsafe.Pointer(replyMsg)))
 		case conn := <-Manager.Unregister: // 断开连接
-			log.Printf("连接失败:%v",conn.ID)
-			if _,ok := Manager.Clients[conn.ID];ok{
+			log.Printf("连接失败:%v", conn.ID)
+			if _, ok := Manager.Clients[conn.ID]; ok {
 				replyMsg := &ReplyMsg{
 					Code:    e.WebsocketEnd,
 					Content: "连接已断开",
 				}
 				_ = conn.Socket.WriteMessage(websocket.TextMessage, *(*[]byte)(unsafe.Pointer(replyMsg)))
 				close(conn.Send)
-				delete(Manager.Clients,conn.ID)
+				delete(Manager.Clients, conn.ID)
 			}
-			//广播信息
-			case broadcast := <- Manager.Broadcast:
-				message := broadcast.Message
-				sendId := broadcast.Client.SendID
-				flag := false // 默认对方不在线
-				for id,conn := range Manager.Clients {
-					if id != sendId {
-						continue
-					}
-					select {
-					case conn.Send <- message:
-						flag = true
-					default:
-						close(conn.Send)
-						delete(Manager.Clients,conn.ID)
-					}
+		//广播信息
+		case broadcast := <-Manager.Broadcast:
+			message := broadcast.Message
+			sendId := broadcast.Client.SendID
+			flag := false // 默认对方不在线
+			for id, conn := range Manager.Clients {
+				if id != sendId {
+					continue
 				}
-				id := broadcast.Client.ID
-				if flag {
-					log.Println("对方在线应答")
-					replyMsg := &ReplyMsg{
-						Code:    e.WebsocketOnlineReply,
-						Content: "对方在线应答",
-					}
-					_ = broadcast.Client.Socket.WriteMessage(websocket.TextMessage,*(*[]byte)(unsafe.Pointer(replyMsg)))
-					err := InsertMsg(conf.MongoDBName,id, string(message),1,int64(3*month))
-					if err != nil {
-						fmt.Println("InsertOneMsg Err",err)
-					}
-				}else{
-					log.Println("对方不在线")
-					replyMsg := &ReplyMsg{
-						Code:    e.WebsocketOfflineReply,
-						Content: "对方不在线应答",
-					}
-					_ = broadcast.Client.Socket.WriteMessage(websocket.TextMessage,*(*[]byte)(unsafe.Pointer(replyMsg)))
-					err := InsertMsg(conf.MongoDBName,id, string(message),0,int64(3*month))
-					if err != nil {
-						fmt.Println("InsertOneMsg Err",err)
-					}
+				select {
+				case conn.Send <- message:
+					flag = true
+				default:
+					close(conn.Send)
+					delete(Manager.Clients, conn.ID)
 				}
+			}
+			id := broadcast.Client.ID
+			if flag {
+				log.Println("对方在线应答")
+				replyMsg := &ReplyMsg{
+					Code:    e.WebsocketOnlineReply,
+					Content: "对方在线应答",
+				}
+				_ = broadcast.Client.Socket.WriteMessage(websocket.TextMessage, *(*[]byte)(unsafe.Pointer(replyMsg)))
+				err := InsertMsg(conf.MongoDBName, id, string(message), 1, int64(3*month))
+				if err != nil {
+					fmt.Println("InsertOneMsg Err", err)
+				}
+			} else {
+				log.Println("对方不在线")
+				replyMsg := &ReplyMsg{
+					Code:    e.WebsocketOfflineReply,
+					Content: "对方不在线应答",
+				}
+				_ = broadcast.Client.Socket.WriteMessage(websocket.TextMessage, *(*[]byte)(unsafe.Pointer(replyMsg)))
+				err := InsertMsg(conf.MongoDBName, id, string(message), 0, int64(3*month))
+				if err != nil {
+					fmt.Println("InsertOneMsg Err", err)
+				}
+			}
 		}
 	}
 }
